e7e561e01090a5d0ebf51c9fa17ada376a9a6bf6
kafene <kafene@users.noreply.github.com>
2017-04-18 04:46:10 -0400

    add iframe sandboxing security

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const iframe = document.createElement("iframe");
+    iframe.sandbox = "allow-popups allow-forms allow-scripts allow-same-origin";
     iframe.src = "https://www.inoreader.com/";
     document.body.appendChild(iframe);
 });
